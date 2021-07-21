6a8bad63f43a0479e5c4419b1ef251876bd9efb0
Francisca Escamilla <francis.escamilla@gmail.com>
2021-07-20 22:06:51 -0500

    working on issues loading the data into index.html

---
+++ Mission_to_Mars/app.py
@@ -0,0 +1,34 @@
+#import dependencies and scrape function
+from flask import Flask, render_template, redirect
+from flask_pymongo import PyMongo
+import scrape_mars
+
+#create instance of flask
+app = Flask(__name__)
+
+# mongo connection
+mongo = PyMongo(app, uri="mongodb://localhost:27017/mars_app")
+mars_data = mongo.db.mars_data
+
+@app.route("/")
+def index():
+
+    # Find one record of data from the mongo database
+    mars_info = mongo.db.mars_data.find_one()
+
+    # Return template and data
+    return render_template("index.html", mars_info=mars_info)
+
+
+@app.route("/scrape")
+def scrape():
+    # call the scrape function
+    mars_data = scrape_mars.scrape_info()
+
+    # update the Mongo DB
+    mongo.db.mars_data.update({}, mars_data, upsert=True)
+
+    return redirect("/")
+
+if __name__=="__main__":
+    app.run(debug=True)
